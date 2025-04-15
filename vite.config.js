65ea07c418058f46f3f2bdc7ac36b525633540d0
fanfuda <fhtdfff@qq.com>
2025-04-16 00:45:06 +0800

    feat(vite): 添加 mock 数据服务
    - 新增 vite 插件 viteMockServer 用于模拟后端数据
    - 在服务器配置中添加 /api/list 接口的 mock 数据
    - 使用 mockjs 库生成模拟数据
    - 在请求中处理查询参数并返回相应的 JSON 数据

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import mockjs from 'mockjs';
+import url from 'node:url';
+// 编写一个vite插件 用于mock数据
+const viteMockServer = () => {
+  return {
+    name: 'vite-mock-server',
+    configureServer: (server) => {
+      server.middlewares.use('/api/list', (req, res) => {
+        // 处理请求
+        console.log('请求路径：', req.url);
+        // url.parse(req.originalUrl, true);  ture 返回的对象 不加true 返回字符串
+        const parseUrl = url.parse(req.originalUrl, true).query;
+        const data = mockjs.mock({
+          'list|300': [
+            {
+              'id|+1': 1,
+              name: parseUrl.keyWord,
+              address: '@county(true)',
+            },
+          ],
+        });
+        //返回头 信息
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data));
+      });
+    },
+  };
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    port:5388,
-  }
-})
+  plugins: [react(), viteMockServer()],
+  server: {
+    port: 5388,
+  },
+});
